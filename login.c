5bbe9289d04c222b481cfe9357d6b62b494b915d
wostlund <wostlund@stuy.edu>
2017-01-18 10:55:27 -0500

    Added login functions

---
+++ login.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+char* cypher(char* x){
+  int i = 0;
+  char[strlen(x)] ans;
+  while(*(x+i)){
+    ans[i] = (*(x+i) * 3) % 128;
+  }
+  return ans;
+}
+
+int checkUsername(char* x){
+  int i;
+  for(i = 0; i < strlen(x); i++){
+    if(*(x + i) == ':'){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int register(char* username, char* password) {
+  umask(0000);
+  char[] x = ":";
+  char[] newLine = "\n";
+  int f = open("users.txt", O_APPEND | O_RDWR, 0666);
+  printf("\nRegistering User...\n\n");
+  if(checkUsername(username) && checkUsername(password)){
+    write(f, username, sizeof(username));
+    write(f, x, sizeof(x));
+    write(f, cypher(password), sizeof(password));
+    write(f, newLine, sizeof(newLine));
+    return 1;
+  }
+  return 0;
+}
+
+int login(char* username, char* password) {
+  umask(0000);
+  char *a = calloc(10000, 1);
+  int f = open("users.txt",  O_RDONLY, 0666);
+  printf("\nLogging In User...\n\n");
+  if(checkUsername(username) && checkUsername(password)){
+    read(f, a, sizeof(a));
+    while(*a){
+      char* k = strsep(a, '\n')
+      if(strcmp(strsep(k, ':'), username) == 0){
+	if(strcmp(k, cypher(password)) == 0){
+	  return 1;
+	}
+      }
+    }
+  }
+  return 0;
+}
