74b71a8d72c41a1e9fc38eef8aecd030015edada
cowson <nielsgraham@gmail.com>
2017-01-18 10:50:07 -0500

    moved things around and added sever code

--- client.c
+++ client.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-#include "networking.h"
-
-int main( int argc, char *argv[] ) {
-
-  char *host;
-  if (argc != 2 ) {
-    printf("host not specified, conneting to 127.0.0.1\n");
-    host = "127.0.0.1";
+void error_check( int i, char *s ) {
+  if ( i < 0 ) {
+    printf("%d\n", i);
+    printf("[%s] error %d: %s\n", s, errno, strerror(errno) );
+    exit(1);
   }
-  else
-    host = argv[1];
-  
-  int sd;
-
-  sd = client_connect( host );
-
-  char buffer[MESSAGE_BUFFER_SIZE];
-  
-  while (1) {
-    printf("enter message: ");
-    fgets( buffer, sizeof(buffer), stdin );
-    char *p = strchr(buffer, '\n');
-    *p = 0;
-  
-    write( sd, buffer, sizeof(buffer) );
-    read( sd, buffer, sizeof(buffer) );
-    printf( "received: %s\n", buffer );
-  }
-  
-  return 0;
 }
 
+
+int client_connect( char *host, int argc, char *argv[] ) {
+  int sd, i;
+
+  sd = socket( AF_INET, SOCK_STREAM, 0 );
+  error_check( sd, "client socket" );
+
+  struct sockaddr_in, sock;
+  sock.sin_family = AF_INET;
+  inet_aton( host, &(sock.sin_addr));
+  portno = atoi(argv[1]);
+  sock.sin_port = htons(portno);
+
+  printf("[client] connecting to: %s\n", host );
+  i = connect( sd, (struct sockaddr *)&sock, sizeof(sock) );
+  error_check( i, "client connect");
+
+  return sd;
+}
