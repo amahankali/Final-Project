367ef188ba5776b8c612f18d6ad842f250047575
cowson <nielsgraham@gmail.com>
2017-01-20 10:55:14 -0500

    write mechanisms for client

--- client.c
+++ client.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <stdio.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -20,6 +21,8 @@ void error_check( int i, char *s ) {
 
 int client_connect( char *host, int argc, char *argv[] ) {
   int sd, i;
+  struct sockaddr_in serv_addr hostent *server;
+  FILE *fp;
 
   sd = socket( AF_INET, SOCK_STREAM, 0 );
   error_check( sd, "client socket" );
@@ -27,12 +30,38 @@ int client_connect( char *host, int argc, char *argv[] ) {
   struct sockaddr_in, sock;
   sock.sin_family = AF_INET;
   inet_aton( host, &(sock.sin_addr));
-  portno = atoi(argv[1]);
-  sock.sin_port = htons(portno);
+  portno = atoi(argv[2]);
+
+  server = gethostbyname(argv[1]);
+   if (server == NULL) {
+       return -1;
+   }
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
+   serv_addr.sin_port = htons(portno);
+   if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+     return -1;
+
+
 
   printf("[client] connecting to: %s\n", host );
   i = connect( sd, (struct sockaddr *)&sock, sizeof(sock) );
   error_check( i, "client connect");
 
+  bzero(buffer,256);
+  fp = fopen(argc[3], "r");
+  fscanf(fp, "%s", buff);
+
+  n = write(sockfd,buffer,strlen(buffer));
+  if (n < 0)
+        return -1;
+  bzero(buffer,256);
+  n = read(sockfd,buffer,255);
+  if (n < 0)
+    return -1;
+  printf("%s\n",buffer);
+  return 0;
+}
   return sd;
 }
