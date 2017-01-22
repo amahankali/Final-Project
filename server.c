ece8a965fcdd94565ee7d92d00c0c422bf79c5a2
cowson <nielsgraham@gmail.com>
2017-01-22 15:24:28 -0500

    code

--- server.c
+++ server.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-
+#include  <fcntl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -37,19 +37,53 @@ int server_setup(int argc, char *argv[]) {
 }
 
 int server_connect(int sd) {
-  int connection, i;
+  int newsockfd, i;
+  struct sockaddr_in sock1, cli_addr;
+  char buffer[256]
 
   i = listen(sd, 5);
   error_check( i, "server listen" );
 
-  struct sockaddr_in sock1, cli_addr;
   unsigned int sock1_len = sizeof(sock1);
-  connection = accept( sd, (struct sockaddr *)&sock1, &sock1_len );
-  error_check( connection, "server accept" );
+  newsockfd = accept( sd, (struct sockaddr *)&sock1, &sock1_len );
+  error_check( newsockfd, "server accept" );
 
   printf("[server] connected to %s\n", inet_ntoa( sock1.sin_addr ) );
 
+  bzero(buffer,256);
+  n = read(newsockfd,buffer,255);
+
+  char subbuff[15];
+  memcpy( subbuff, &buffer, 14 );
+  subbuff[15] = '\0';
+  if(strcmp(buffer, "$gitProject -e") == 0){
+
+  }
+
+
   return connection;
+}
+
+char sendfile(char name){
+FILE *fp;
+long lSize;
+char *buffer;
+
+fp = fopen ( "blah.txt" , "rb" );
+if( !fp ) perror("blah.txt"),exit(1);
+
+fseek( fp , 0L , SEEK_END);
+lSize = ftell( fp );
+rewind( fp );
+
+/* allocate memory for entire content */
+buffer = calloc( 1, lSize+1 );
+if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
+
+/* copy the file into the buffer */
+if( 1!=fread( buffer , lSize, 1 , fp) )
+  fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
+
 }
 
 int main() {
@@ -70,7 +104,7 @@ int main() {
     //this is the main code - use functions from our other files
     //there will be one server executable and one client executable
 
-    
+
 
 
   }
