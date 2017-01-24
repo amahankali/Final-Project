58edec88430e69ec40dae23f7e19d8e055ac4887
arvind <amahankali@stuy.edu>
2017-01-24 02:07:27 -0500

    changed port

--- server/constant.h
+++ server/constant.h
@@ -1,5 +1,5 @@
 #define TESTIP "24.103.0.186" //just to be clear, IP ADDRESS OF SERVER - clyde.stuy.edu
-#define TESTPORT "22"
+#define TESTPORT "15324"
 #define MAXFILESIZE 5000 //character limit
 #define MAXMESSAGE 100 //in general, maximum length of any single communication using a socket - other than file
 #define COMMANDSIZE 17
