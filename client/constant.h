440691b72bf826281721346c0b60df0ba195ec5e
arvind <amahankali@stuy.edu>
2017-01-24 03:06:34 -0500

    t

--- client/constant.h
+++ client/constant.h
@@ -1,4 +1,4 @@
-#define TESTIP "24.103.0.186" //just to be clear, IP ADDRESS OF SERVER - clyde.stuy.edu
+#define TESTIP "127.0.0.1" //just to be clear, IP ADDRESS OF SERVER - clyde.stuy.edu
 #define TESTPORT "5001"
 #define MAXFILESIZE 5000 //character limit
 #define MAXMESSAGE 100 //in general, maximum length of any single communication using a socket - other than file
